631713add037f136fd5e6715e7a555dcba825a14
attmhd <2022610013.attan@itp.ac.id>
2024-02-29 01:12:41 +0700

    Update Lat Bilangan Prima

--- M1/lat1.c
+++ M1/lat1.c
@@ -5,20 +5,7 @@
 #include <stdbool.h>
 
 //Deklarasi variabel
-int n;
-char c[1];
-bool isPrima = true, isStart = true; 
-
-//Fungsi
-int choices(){
-        printf("Lagi ? [Y/N] : ");
-        scanf( "%c", &c );
-
-        if ((c == 'Y') || (c == 'y'))   isStart = true;
-        else if ((c == 'N') || (c == 'n'))  isStart = false;
-        else printf("Pilihan yang anda Masukkan Out Of Context \n");
-        return isStart;
-}
+int n, i;
 
 //Fungsi untuk tampilan head dan input angka
 int head(){
@@ -34,27 +21,28 @@ int head(){
 //Fungsi untuk formula menentukan bilangan prima
 int formula(){
 
-    if (n == 1) isPrima = false;
-    else if ((n % 2 == 1 )&&(2 >= n /2))    isPrima = true;
-
-    return isPrima;
+    if (n == 1) return false;
+    else{
+        for ( i = 2; i  <= n /2; i++)
+        {
+            if (n % i == 0) {
+                return false;
+                break;
+            }
+        }
+        return true;
+    }
 }
 
 //Prosedur untuk cek bilangan Prima
 void isPrimar(){
-    
-    printf("\n");
-    if (isPrima == true)    printf("%i merupakan bilangan prima \n",n);
+    bool isPrima = formula();
+
+    if (isPrima == true)   printf("\n%i merupakan bilangan prima \n",n);
     else    printf("%i bukan bilangan prima \n",n);
 }
 
 int main(){
-    do
-    {
         head();
-        formula();
         isPrimar();
-        choices();
-
-    } while (isStart!= false );
 }
