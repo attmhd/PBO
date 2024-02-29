f41c6a0b4d3e65447e2a0ce185b0f5e8c922f902
attmhd <2022610013.attan@itp.ac.id>
2024-02-29 15:56:36 +0700

    Latihan 2

---
+++ M1/lat2.c
@@ -0,0 +1,31 @@
+//Program menghitung luas, keliling dan volume 
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#define phi 3.14
+
+int choice;
+
+int menu(){
+    system("clear");
+    printf("           Program menghitung luas, keliling dan volume           \n")  ;
+    printf("===================================================================\n")  ;
+    printf("1. Luas dan Keliling Lingkaran\n");
+    printf("2. Luas dan Keliling Bujur Sangkar");
+    printf("3. Luas dan Keliling Persegi Panjang");
+    printf("4. Volume Kubus");
+    printf("5. Volume Tabung");
+
+    scanf("%d", &choice);
+
+    return choice; 
+}
+
+int lkLingkaran(int r){
+    int luas = phi * pow(r,2);
+    int keliling = 2 * phi * r;
+}
+
+int main(){
+
+}
